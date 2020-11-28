d0f2d40d0113ea4905ccbace18c851eabdf96684
sundowndev <raphael@crvx.fr>
2020-11-28 23:13:57 +0400

    feat: init style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>
+  <div class="container">
+    <div class="header">
+      <NuxtLink to="/"
+        ><img id="logo" src="~/assets/images/logo.svg" alt=""
+      /></NuxtLink>
+
+      <div class="menu">
+        <ul>
+          <li>
+            <NuxtLink to="/">Products</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Pricing</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Open Source</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">GitHub</NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+
     <Nuxt />
   </div>
 </template>
 
-<style></style>
+<style lang="scss"></style>
