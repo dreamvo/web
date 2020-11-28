e97579be69414a52bd5685eb47111870f258594b
sundowndev <raphael@crvx.fr>
2020-11-28 18:40:16 +0400

    fix: listen to all interface instead of localhost

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,11 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost
+  },
+
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
 
