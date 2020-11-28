d33ef66c900f9e53c0b38aab9253ed5b7536c73b
sundowndev <raphael@crvx.fr>
2020-11-28 17:51:28 +0400

    refactor: redirect home to github for now

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,16 @@
-<template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">Dreamvo</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
-</template>
+<template><p>Redirecting to github.com/dreamvo...</p></template>
 
 <script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
+export default {
+  head: {
+    title: 'Dreamvo',
+    meta: [
+      { charset: 'utf-8' },
+      {
+        'http-equiv': 'Refresh',
+        content: "0; url='https://github.com/dreamvo'",
+      },
+    ],
+  },
+}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
